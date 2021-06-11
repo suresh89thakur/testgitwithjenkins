c22b6f521a557ab54d48f16e0a20c7a84db1cd16
suresh@apscentral.com <suresh@apscentral.com>
2021-06-11 10:54:24 +0530

    aa

--- testgitwithjenkins/Controllers/WeatherForecastController.cs
+++ testgitwithjenkins/Controllers/WeatherForecastController.cs
@@ -13,7 +13,7 @@ namespace testgitwithjenkins.Controllers
     {
         private static readonly string[] Summaries = new[]
         {
-            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching", "Melt"
         };
 
         private readonly ILogger<WeatherForecastController> _logger;
